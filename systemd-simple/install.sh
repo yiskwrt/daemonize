4b5c4f35ead181fa803bc2333b8e1b75b595bcf3
Yuji Ishikawa <yuji2iskwutrt@gmail.com>
2022-05-05 11:55:55 +0900

    add unit script for simple systemd daemon

---
+++ systemd-simple/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## build application without daemonize routine
+make -C ../src clean mywebserver
+
+## copy executable
+sudo mkdir /opt/mywebserver
+sudo cp ../src/mywebserver /opt/mywebserver
+
+## link script
+SCRIPT=$(pwd)/mywebserver.service
+sudo rm -f /etc/systemd/system/mywebserver.service
+(cd /etc/systemd/system; sudo ln -sf ${SCRIPT})
